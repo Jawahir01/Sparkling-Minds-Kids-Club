65c9515c437596b1eb1f938b4343b3fb7a5dd176
Jawahir01 <>
2024-01-26 12:21:12 +0000

    Add Register route

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
